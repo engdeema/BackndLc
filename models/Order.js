4bed3e1903480abaa8f7f3120aed9004077e25d1
Deema Burhama <eng.dburhama@gmail.com>
2022-03-14 19:21:34 +0300

    order phase 2

--- models/Order.js
+++ models/Order.js
@@ -1,12 +1,16 @@
 const { model, Schema } = require("mongoose");
-const OrderSchema = Schema({
-  name: { type: String },
-  image: { type: String },
-  description: { type: String },
-  quantity: {
-    type: Number,
-    default: 5,
+const OrderSchema = Schema(
+  {
+    sweet: { type: Schema.Types.ObjectId, ref: "Sweet" },
+    quantity: {
+      type: Number,
+      max: 5,
+    },
+    processed: { type: Boolean, default: false },
+    // if some one has purchase 4 sweets and another person wants 3 more
+    // and your max is 5 orders, so this one will check timing of the purchase
   },
-});
+  { timeStamps: true }
+);
 
 module.exports = model("Order", OrderSchema);
